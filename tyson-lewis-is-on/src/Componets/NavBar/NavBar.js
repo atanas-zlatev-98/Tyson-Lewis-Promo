997c0c7169b2002f39fa309b666c657f0cafa673
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-05-08 18:40:03 +0300

    fixed some mobile views

--- tyson-lewis-is-on/src/Componets/NavBar/NavBar.js
+++ tyson-lewis-is-on/src/Componets/NavBar/NavBar.js
@@ -4,19 +4,27 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../Style/Assets/Images/Logo/logo.png';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 const NavBar = () => {
+
+  const [changeColor,setChangeColor] = useState(false);
+
+  const ChangeNavBG = () => {
+    setChangeColor(current => !current);
+  }
+
     return(
       <Container className='navbar-container p-0 m-0'>
-        <Navbar className='navbar-color p-0' expand="lg">
+        <Navbar style={{backgroundColor: changeColor ? 'black' : ''}} className="navbar-color p-0" expand="lg">
         <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className='p-0 m-0'/>
+          <Navbar.Toggle onClick={ChangeNavBG} aria-controls="basic-navbar-nav" className='p-0 m-0'/>
           <Navbar.Collapse id="basic-navbar-nav" className='p-0 m-0'>
             <Nav className="me-auto nav-links d-flex justify-content-center  align-items-center">
               <NavLink className="links-nav" to="#home">Home</NavLink>
-              <NavLink className="links-nav" to="#link">Pay-Per-View</NavLink>
+              <NavLink className="links-nav" to="#link">Team Tyson</NavLink>
               <Navbar.Brand href="#home" className='brand'><img style={{height:"100px", width:"100px"}} src={logo} alt='navbar logo'></img></Navbar.Brand>
-              <NavLink className="links-nav"  to="#">Location</NavLink>
-              <NavLink className="links-nav" to="#">Contacts</NavLink>
+              <NavLink className="links-nav"  to="#">Team Lewis</NavLink>
+              <NavLink className="links-nav" to="#">PPV</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- tyson-lewis-is-on/src/Pages/Home.js
+++ tyson-lewis-is-on/src/Pages/Home.js
@@ -1,54 +1,57 @@
 import {Button, Container} from 'react-bootstrap';
-
+//import lewis from '../Style/Assets/Images/Lewis.png';
 const Home = () => {
     return(
         <Container className='home p-0 m-0'>
             <Container className='posters p-0 m-0 d-flex flex-column'>
 
-             <Container className='poster-m p-0 m-0'>
-                    <Container className='poster-img p-0 m-0 pb-5 d-flex flex-column justify-content-end '>
-                <Container className='poster-center d-flex align-items-center justify-content-center flex-column'>
-                    <p className='top-text-poster m-0 p-0 orange'>WHERE WILL YOU BE WHEN HISTORY GOES DOWN?</p>
-                    <h1 className='imp m-0 p-0'>
-                        <span className='lewis-name'>LENNOX LEW<span className='orange'>IS</span></span><br/> 
-                        <span className='tyson-name'>VS. MIKE TYS<span className='orange'>ON</span></span>
-                    </h1>
-                    <p className='bottom-text-poster m-0 p-0 orange'>WORLD HEAVYWEIGHT CHAMPIONSHIP</p>
-                    <p className='imp m-0 p-0 last-poster'>SEE IT LIVE! SATURDAY, JUNE 8, 9PM ET/6PM PT</p>
+                <Container className='poster-d p-0 m-0'>
+                    <Container className='Lewis'>
 
-                    <Container className='learn-more m-2 d-flex justify-content-center flex-row'>
-                        <Button variant='none' className='custom-btn text-white m-3'>Learn More</Button>
-                        <Button variant='none' className='custom-btn text-white m-3'>Buy PPV</Button>
                     </Container>
 
-                </Container>
+                    <Container className='middle-txt'>
+
+                        <p className='top-text-poster m-0 p-0 orange'>WHERE WILL YOU BE WHEN HISTORY GOES DOWN?</p>
+                            <h1 className='imp m-0 p-0'>
+                                <span className='lewis-name'>LENNOX LEW<span className='orange'>IS</span></span><br/> 
+                                <span className='tyson-name'>VS. MIKE TYS<span className='orange'>ON</span></span>
+                            </h1>
+                        <p className='bottom-text-poster m-0 p-0 orange'>WORLD HEAVYWEIGHT CHAMPIONSHIP</p>
+                        <p className='imp m-0 p-0 last-poster'>SEE IT LIVE! SATURDAY, JUNE 8, 9PM ET/6PM PT</p>
+
+                        <Container className='learn-more m-2 d-flex justify-content-center flex-row'>
+                            <Button variant='none' className='custom-btn text-white m-3'>Learn More</Button>
+                            <Button variant='none' className='custom-btn text-white m-3'>Buy PPV</Button>
+                        </Container>
+
                     </Container>
+                    <Container className='Tyson'></Container>
                 </Container>
 
-                <Container className='poster p-0 m-0'>
+                <Container className='poster-m m-0 p-0'>
+                    <Container className='poster-m-img p-0 m-0'>
 
-                <Container className='Lewis'></Container>
+                    <Container className='middle-txt'>
 
-                <Container className='poster-center d-flex align-items-center justify-content-center flex-column'>
-                    <p className='top-text-poster m-0 p-0 orange'>WHERE WILL YOU BE WHEN HISTORY GOES DOWN?</p>
-                    <h1 className='imp m-0 p-0'>
-                        <span className='lewis-name'>LENNOX LEW<span className='orange'>IS</span></span><br/> 
-                        <span className='tyson-name'>VS. MIKE TYS<span className='orange'>ON</span></span>
-                    </h1>
-                    <p className='bottom-text-poster m-0 p-0 orange'>WORLD HEAVYWEIGHT CHAMPIONSHIP</p>
-                    <p className='imp m-0 p-0 last-poster'>SEE IT LIVE! SATURDAY, JUNE 8, 9PM ET/6PM PT</p>
+<p className='top-text-poster m-0 p-0 orange'>WHERE WILL YOU BE WHEN HISTORY GOES DOWN?</p>
+    <h1 className='imp m-0 p-0'>
+        <span className='lewis-name'>LENNOX LEW<span className='orange'>IS</span></span><br/> 
+        <span className='tyson-name'>VS. MIKE TYS<span className='orange'>ON</span></span>
+    </h1>
+<p className='bottom-text-poster m-0 p-0 orange'>WORLD HEAVYWEIGHT CHAMPIONSHIP</p>
+<p className='imp m-0 p-0 last-poster'>SEE IT LIVE! SATURDAY, JUNE 8, 9PM ET/6PM PT</p>
 
-                    <Container className='learn-more m-2 d-flex justify-content-center flex-row'>
-                        <Button variant='none' className='custom-btn text-white m-3'>Learn More</Button>
-                        <Button variant='none' className='custom-btn text-white m-3'>Buy PPV</Button>
+<Container className='learn-more m-2 d-flex justify-content-center flex-row'>
+    <Button variant='none' className='custom-btn text-white m-3'>Learn More</Button>
+    <Button variant='none' className='custom-btn text-white m-3'>Buy PPV</Button>
+</Container>
+
+</Container>
+
                     </Container>
-
+                    
                 </Container>
-
-                <Container className='Tyson'></Container>
-
-             </Container>
-
 
             </Container>
 
